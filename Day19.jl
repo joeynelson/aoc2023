79c0b0ea014772fc7241511fa540f320c1b2ba53
Joey Nelson <joey@joescan.com>
2025-09-11 16:56:50 -0700

    Solved 19b, adding 20

--- Day19.jl
+++ Day19.jl
@@ -1,6 +1,6 @@
 function read_input19(file_path::AbstractString = "Day19.txt")
-    in = read(file_path, String)
-    sections = split(in, "\n\n")
+    input =  filter(c -> c != '\r', read(file_path, String))
+    sections = split(input, "\n\n")
 
     rules = split.(sections[1], "\n")
     parts = filter(!isempty, split.(sections[2], "\n"))
@@ -11,7 +11,7 @@ function parse_rule(rule::AbstractString)
     name, flows, default = match(r"(\w+)\{(.+),(\w+)\}",rule)
    
     flows = split(flows, ",")
-    flows = map(m -> tuple(m[1],m[2] == ">" ? !isless : isless,parse(Int,m[3]),m[4]) ,match.(Ref(r"(\w+)([\<\>])(\d+)\:(\w+)"), flows))
+    flows = map(m -> tuple(m[1],m[2] == ">" ? (a,b) -> a > b : (a,b) -> a < b,parse(Int,m[3]),m[4]) ,match.(Ref(r"(\w+)([\<\>])(\d+)\:(\w+)"), flows))
 
     return (name, flows, default)
 end
@@ -43,6 +43,31 @@ function apply_workflow(name::AbstractString, workflows::Dict, part::Dict)
     end
 end
 
+function measure_workflows(name::AbstractString, workflows::Dict, params::Dict = 
+        Dict("x" => Set(1:4000), "m" => Set(1:4000), "a" => Set(1:4000), "s" => Set(1:4000)))
+
+    params = deepcopy(params)
+    good_params = 0
+        
+    (flows, default) = workflows[name]
+    for (name, condition, value, result) in flows
+        condparams = deepcopy(params)
+        condparams[name] = Set(filter(x -> condition(x, value), condparams[name]))
+        params[name] = Set(filter(x -> !condition(x, value), params[name]))
+        if result == "A"
+            good_params += prod(length.(values(condparams)))
+        elseif result != "R"
+            good_params += measure_workflows(result, workflows, condparams)
+        end
+    end
+    if default == "A"
+        good_params += prod(length.(values(params)))
+    elseif default != "R"
+        good_params += measure_workflows(default, workflows, params)
+    end
+    return good_params
+end
+
 function solve_19a()
     rules, parts = read_input19()
 
@@ -50,9 +75,13 @@ function solve_19a()
     parts = map(parse_part, parts)
     workflows = Dict(r[1] => (r[2],r[3]) for r in rules)
     return sum(apply_workflow("in", workflows, part) ? sum(values(part)) : 0 for part in parts)  
+end
 
-    
+function solve_19b()
+    rules, parts = read_input19()
 
-
-   
+    rules = map(parse_rule, rules)
+    parts = map(parse_part, parts)
+    workflows = Dict(r[1] => (r[2],r[3]) for r in rules)
+    return measure_workflows("in",workflows)
 end
